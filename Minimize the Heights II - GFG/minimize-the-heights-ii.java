a79a1409c1463027ca1915725af0fdcd388b5f49
Alfi Naaz <101350712+alfinaaz@users.noreply.github.com>
2022-04-15 01:26:27 +0530

    Added solution - LeetHub

--- Minimize the Heights II - GFG/minimize-the-heights-ii.java
+++ Minimize the Heights II - GFG/minimize-the-heights-ii.java
@@ -38,7 +38,7 @@ class Solution {
         int a= arr[0]+k;
         int b= arr[n-1]-k;
         
-        int res= arr[n-1]-arr[0];
+        int res = arr[n-1]-arr[0];
         for(int i=1;i<n;i++)
         {
             if(arr[i]>=k){
