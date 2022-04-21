c79d8942eecbfa709e518bc5a4a8fed23d388ccf
Alfi Naaz <101350712+alfinaaz@users.noreply.github.com>
2022-04-22 00:03:04 +0530

    Added solution - LeetHub

--- Count Inversions - GFG/count-inversions.java
+++ Count Inversions - GFG/count-inversions.java
@@ -49,7 +49,7 @@ class Solution
  
             invCount = mergeSort(arr, left, mid);
             invCount += mergeSort(arr, mid + 1, right);
-            invCount += merge(arr, left, mid + 1, right);
+            invCount += merge(arr, left, mid+1, right);
         }
         return invCount;
     }
@@ -61,27 +61,27 @@ class Solution
         while ((i < mid) && (j <= right)) {
             if (arr[i] <= arr[j]) {
                 temp[k] = arr[i];
-                ++k;
-                ++i;
+                k++;
+                i++;
             } 
             else {
                 temp[k] = arr[j];
                 invCount += (mid - i);
-                ++k;
-                ++j;
+                k++;
+                j++;
             }
         }
  
         while (i < mid) {
             temp[k] = arr[i];
-            ++k;
-            ++i;
+            k++;
+            i++;
         }
  
         while (j <= right) {
             temp[k] = arr[j];
-            ++k;
-            ++j;
+            k++;
+            j++;
         }
  
         for (i = left, k = 0; i <= right; i++, k++) {
