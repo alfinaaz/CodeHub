2e9c075246a6751030ad61b0c370bb38a7c44610
Alfi Naaz <101350712+alfinaaz@users.noreply.github.com>
2022-04-23 04:42:36 +0530

    Time: 3 ms (96.94%), Space: 45.1 MB (15.04%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n= nums.length;
+           	   	if (nums == null | nums.length == 0)
+		return 0;
+	int  len = 0;
+	int[] increasingSequence = new int[n];
+	increasingSequence[len++] = nums[0];
+	for (int i = 1; i < n; i++) {
+		if (nums[i] > increasingSequence[len - 1])
+			increasingSequence[len++] = nums[i];
+		else {
+			int position = findPositionToReplace(increasingSequence, 0, len - 1, nums[i]);
+			increasingSequence[position] = nums[i];
+		}
+		
+	}
+		return len;
+    }
+       public static int findPositionToReplace(int[] a, int low, int high, int x) {
+	int mid;
+	while (low <= high) {
+		mid = low + (high - low) / 2;
+		if (a[mid] == x)
+			return mid;
+		else if (a[mid] > x)
+			high = mid - 1;
+		else
+			low = mid + 1;
+	}
+	return low;
+    
+       }
+}
