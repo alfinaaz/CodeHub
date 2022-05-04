b9ca3b35e41bc00a3e4b307dc3a6b3e6b88c97e9
Alfi Naaz <101350712+alfinaaz@users.noreply.github.com>
2022-05-05 04:04:05 +0530

    Time: 2 ms (71.67%), Space: 45 MB (41.22%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -1,47 +1,17 @@
 class Solution {
-    public int maxProduct(int[] arr) {
-        
-        int flag=0;
-        
-        if(arr.length==1)
-            return arr[0];
-        
-        int maxEnd=1,minEnd=1,max=Integer.MIN_VALUE;
-        
-        for(int i=1;i<arr.length;i++)
-        {
-            
-           if(!((arr[i]<0 && arr[i-1]==0)|| (arr[i]==0 && arr[i-1]<0)) || arr[i]==0 && arr[i-1]==0)
-           {
-               flag=1;
-               break;
-           }
-            
-            
-        }
-        if(flag==0)
+  public int maxProduct(int[] A) {
+        if (A == null || A.length == 0) {
             return 0;
-        
-        
-      
-        
-        for(int i=0;i<arr.length;i++)
-        {
-            if(arr[i]==0)
-            {
-                maxEnd=1;
-                minEnd=1;
-                continue;
-            }
-            
-            int temp=maxEnd;
-            maxEnd=Math.max(Math.max(maxEnd*arr[i],minEnd*arr[i]),arr[i]);
-            minEnd=Math.min(Math.min(temp*arr[i],minEnd*arr[i]),arr[i]);
-            
-            max=Math.max(max,maxEnd);
         }
-        
-        return max;
-        
-    }
+        int max = A[0], min = A[0], result = A[0];
+        for (int i = 1; i < A.length; i++) {
+            int temp = max;
+            max = Math.max(Math.max(max * A[i], min * A[i]), A[i]);
+            min = Math.min(Math.min(temp * A[i], min * A[i]), A[i]);
+            if (max > result) {
+                result = max;
+            }
+        }
+        return result;
+  }
 }
