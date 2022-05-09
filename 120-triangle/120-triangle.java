5d135d94a52df4a36df95a34a54c3fb88ad45dee
Alfi Naaz <101350712+alfinaaz@users.noreply.github.com>
2022-05-10 03:01:06 +0530

    Time: 10 ms (17.49%), Space: 50.7 MB (5.04%) - LeetHub

---
+++ 120-triangle/120-triangle.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int[][] mem= new int[201][201];
+        
+        for(int i=0;i<mem.length;i++)
+        {
+           Arrays.fill(mem[i],-1);
+        }
+        return helper(triangle,0,0,mem);
+    }
+    
+    public int helper(List<List<Integer>> triangle,int r, int c,int[][] mem)
+    {
+        if(r>=triangle.size())
+            return 0;
+        
+        if(mem[r][c]!=-1)
+            return mem[r][c];
+        
+        mem[r][c]= Math.min(helper(triangle,r+1,c,mem),helper(triangle,r+1,c+1,mem))+triangle.get(r).get(c);
+        return mem[r][c];
+        
+        
+        
+    }
+}
