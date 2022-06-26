b9813965f87b03686c564f62f98b71445b60bc15
Alfi Naaz <101350712+alfinaaz@users.noreply.github.com>
2022-06-27 02:40:30 +0530

    Time: 12 ms (66.61%), Space: 234.4 MB (52.45%) - LeetHub

--- 428-serialize-and-deserialize-n-ary-tree/428-serialize-and-deserialize-n-ary-tree.java
+++ 428-serialize-and-deserialize-n-ary-tree/428-serialize-and-deserialize-n-ary-tree.java
@@ -19,66 +19,92 @@ class Node {
 
 class Codec {
     // Encodes a tree to a single string.
-    public String serialize(Node root) {
+    class WrappableInt{
+        private int value;
         
-        StringBuilder sb= new StringBuilder();
-        serializehelper(root,sb);
-        return sb.toString();
+        public WrappableInt(int x)
+        {
+            this.value=x;
+        }
+        public int getValue()
+        {
+            return this.value;
+        }
         
-    }
-     public StringBuilder serializehelper(Node root, StringBuilder sb) {
+        public void increment()
+        {
+            this.value++;
+        }
         
-       if(root==null)
-       {
-           return sb;
-       }     
-     sb.append(root.val);
-     sb.append(",");
-     sb.append(root.children.size());
-     sb.append(",");
-     
-    for(int i=0;i<root.children.size();i++)
-    {
-        serializehelper(root.children.get(i),sb);
     }
     
-         
-         return sb;
+    class DO extends HashMap<Integer, Pair<Integer,Pair<Integer,Node>>>
+    {
+        
     }
-	
+    
+    public String serialize(Node root) {
+        WrappableInt idx = new WrappableInt(1);
+        StringBuilder sb = new StringBuilder();
+         shelper(root,idx,sb,-1);
+        return sb.toString();
+    }
+    public void shelper(Node root,  WrappableInt idx, StringBuilder sb,int parentIndex)
+    {
+        
+        if(root==null)
+            return ;
+        
+        sb.append((char)(idx.getValue()+'0'));
+        sb.append((char)(root.val+'0'));
+        sb.append((char)(parentIndex==-1?'N':parentIndex+'0'));
+        
+        parentIndex=idx.getValue();
+       
+        for(Node child: root.children)
+        {
+             idx.increment();
+             shelper(child,idx,sb,parentIndex);
+        }
+        
+       
+        
+        
+    }
     // Decodes your encoded data to tree.
     public Node deserialize(String data) {
-      //  if(data=="")
-        //    return null;
         
-        String[] arr = data.split(",");
-        List<String> arr1 = new ArrayList<String>(Arrays.asList(arr));
+        if(data.isEmpty())
+            return null;
         
-        return deserializehelper(arr1);
+        DO nodesandParents= new DO();
+       
+        for(int i=0;i<data.length();i=i+3)
+        {
+            int id= data.charAt(i) - '0';
+            int val= data.charAt(i+1) - '0';
+            int pidx= data.charAt(i+2) -'0';
+            Pair<Integer,Pair<Integer,Node>> node = new Pair<Integer,Pair<Integer,Node>>(val,new Pair<Integer,Node>(pidx,new Node(val,new ArrayList<Node>())));
+            
+            nodesandParents.put(id,node);
+            
+        }
         
+        for(int i=3;i<data.length();i+=3)
+        {
+            int id= data.charAt(i)-'0';
+            int val= data.charAt(i+1)-'0';
+            int pidx= data.charAt(i+2)-'0';
+            
+    Node child  = nodesandParents.get(id).getValue().getValue();
+    Node parent= nodesandParents.get(pidx).getValue().getValue();
+            
+            parent.children.add(child);
+            
+        }
+        
+        return nodesandParents.get(1).getValue().getValue();
     }
-     public Node deserializehelper(List<String> arr1) {
-         
-         if(arr1.size()==1)
-             return null;
-         
-         int data = Integer.parseInt(arr1.get(0));
-         int numChild= Integer.parseInt(arr1.get(1));
-         
-         arr1.remove(0);
-         arr1.remove(0);
-         
-         List<Node> children = new ArrayList<>();
-         Node root= new Node(data,children);
-         for(int i=0;i<numChild;i++)
-         {
-             Node childNode= deserializehelper(arr1);
-             root.children.add(childNode);
-         }
-         
-         return root;
-         
-}
 }
 
 // Your Codec object will be instantiated and called as such:
