358cbd232715b661b594f2a0620fdcc7ffca2cea
Alfi Naaz <101350712+alfinaaz@users.noreply.github.com>
2022-07-07 00:53:01 +0530

    Time: 6 ms (83.51%), Space: 49.9 MB (28.87%) - LeetHub

--- 536-construct-binary-tree-from-string/536-construct-binary-tree-from-string.java
+++ 536-construct-binary-tree-from-string/536-construct-binary-tree-from-string.java
@@ -17,53 +17,48 @@ class Solution {
     public TreeNode str2tree(String s) {
         
         return helper(s,0).getKey();
-        
     }
     
-    public Pair<Integer, Integer> getNumber(String s, int index) {
-        
-        boolean isNegative = false;
-        
-        // A negative number
-        if (s.charAt(index) == '-') {
-            isNegative = true;
-            index++;
+    public Pair<Integer,Integer> getNumber(String s , int idx)
+    {
+        boolean isNegative=false;
+        if(s.charAt(idx)=='-')
+        {
+            isNegative= true;
+            idx++;
         }
-            
-        int number = 0;
-        while (index < s.length() && Character.isDigit(s.charAt(index))) {
-            number = number * 10 + (s.charAt(index) - '0');
-            index++;
+        int number=0;
+        while(idx<s.length() && Character.isDigit(s.charAt(idx)))
+        {
+            number= number*10+(s.charAt(idx)-'0');
+            idx++;
         }
         
-        return new Pair<Integer, Integer>(isNegative ? -number : number, index);
-    } 
-    
+        return new Pair<>(isNegative?-number:number,idx);
+    }
+        
     public Pair<TreeNode,Integer> helper(String s, int idx)
     {
         if(idx==s.length())
-            return new Pair<TreeNode,Integer>(null,idx);
-    
+            return new Pair<>(null,idx);
         
-    Pair<Integer,Integer> numData =getNumber(s,idx);
-    TreeNode node = new TreeNode(numData.getKey());
-    idx= numData.getValue();
-    
-    if(idx<s.length() && s.charAt(idx)=='(')
-    {
-        Pair<TreeNode,Integer> l = helper(s,idx+1);
-        node.left=l.getKey();
-        idx= l.getValue();
+        Pair<Integer,Integer> numData = getNumber(s,idx);
+        TreeNode node = new TreeNode(numData.getKey());
+        idx= numData.getValue();
+        
+        if(idx<s.length() && s.charAt(idx)=='(')
+        {
+            Pair<TreeNode,Integer> l = helper(s,idx+1);
+            node.left= l.getKey();
+            idx=l.getValue();
+        }
+         if(idx<s.length() && s.charAt(idx)=='(')
+        {
+            Pair<TreeNode,Integer> r = helper(s,idx+1);
+            node.right= r.getKey();
+            idx=r.getValue();
+        }
+        
+        return new Pair<>(node,idx<s.length() && s.charAt(idx)==')'?idx+1:idx);
     }
-        
-    if(idx<s.length() && s.charAt(idx)=='(')
-    {
-        Pair<TreeNode,Integer> r = helper(s,idx+1);
-        node.right=r.getKey();
-        idx= r.getValue();
-    }
-        
-         return new Pair<TreeNode, Integer>(node, idx < s.length() && s.charAt(idx) == ')' ? idx + 1 : idx);
-    
-}
 }
