3f8f3be46822b5791c85e201bf185d2424fc696a
Alfi Naaz <101350712+alfinaaz@users.noreply.github.com>
2022-07-09 21:48:42 +0530

    Time: 6 ms (68.43%), Space: 44.7 MB (32.83%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -15,12 +15,12 @@
  */
 class Solution {
     int count=0;
-    HashMap<Integer,Integer> map= new HashMap<>();
     public int pathSum(TreeNode root, int targetSum) {
-         helper(0,root,targetSum);
+           HashMap<Integer,Integer> map= new HashMap<>();
+         helper(0,root,targetSum,map);
         return count;
     }
-    public  void helper(int curr_sum,TreeNode root,int k)
+    public  void helper(int curr_sum,TreeNode root,int k,HashMap<Integer,Integer> map)
     {
         if(root==null)
                 return;
@@ -31,8 +31,8 @@ class Solution {
         count+= map.getOrDefault((curr_sum+root.val-k),0);
         map.put(curr_sum+root.val,map.getOrDefault((curr_sum+root.val),0)+1);
         
-       helper((curr_sum+root.val),root.left,k);
-       helper((curr_sum+root.val),root.right,k);
+       helper((curr_sum+root.val),root.left,k,map);
+       helper((curr_sum+root.val),root.right,k,map);
         
          map.put((curr_sum+root.val), map.get(curr_sum+root.val) - 1);
     }
