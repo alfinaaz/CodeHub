e2fa0df791bc9e404c1e5d494a95e8bbeba631a9
Alfi Naaz <101350712+alfinaaz@users.noreply.github.com>
2022-07-20 02:10:33 +0530

    Time: 45 ms (5.18%), Space: 54.4 MB (5.20%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.java
@@ -16,28 +16,54 @@
 class Solution {
    // String str2="";
     
-  
+    boolean flag=false;
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         
-        if(root == null) return false;
-        if(isIdentical(root, subRoot)) return true;
+        String str1= serialize(subRoot,"");
+        helper(root,str1);
         
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        if(flag==true)
+            return true;
+        
+        return false;
+    }
+    public String helper(TreeNode root,String str1)
+    {
+         if(root==null)
+         {
+             return "N,";
+         }
+      // str2+=String.valueOf(root.val)+"," ;
+      // str2+= helper(root.left,str1);
+      // str2+= helper(root.right,str1);
+        
+        String l= helper(root.left,str1);
+        String r= helper(root.right,str1);
+        
+        String str2 =root.val+","+l+r;
+        
+        if(str2.equals(str1))
+        {
+            flag=true;
+           
+        }
+        
+        return str2;
+        
     }
     
-    
-    public boolean isIdentical(TreeNode p, TreeNode q){
-        if(p == null && q == null) return true;
+    public String serialize(TreeNode root,String str1)
+    {
+        if(root==null)
+        {
+            str1+="N,";
+            return str1;
+
+        }
+        str1+=(root.val)+",";
+        str1=serialize(root.left,str1);
+        str1=serialize(root.right,str1);
         
-        if(p == null || q == null) return false;
-        
-        if(p.val != q.val) return false;
-        
-        boolean left = isIdentical(p.left, q.left);
-        boolean right = isIdentical(p.right, q.right);
-        
-        
-        
-        return left && right;
+        return str1;
     }
 }
