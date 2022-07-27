0936e42a952db38d4ea7a8c1e129d31f6faf5be1
Alfi Naaz <101350712+alfinaaz@users.noreply.github.com>
2022-07-27 17:57:31 +0530

    Time: 4 ms (5.95%), Space: 42.1 MB (83.03%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -14,28 +14,91 @@
  * }
  */
 class Solution {
+    
+    class Pair<K,V>{
+        
+        K key;
+        V value;
+        
+        public Pair(K key, V value){
+            
+            this.key=key;
+            this.value=value;
+            
+        }
+         public K getKey() {
+        return this.key;
+    }
+     public V getValue() {
+        return this.value;
+    }
+}
     public void flatten(TreeNode root) {
         
-        if(root==null)
-            return;
+      if(root==null)
+          return;
         
-        TreeNode node = root;
-        while(node!=null)
+        int START=1,END=2;
+        TreeNode tailNode=null;
+        
+        Stack<Pair<TreeNode,Integer>> stack= new Stack<Pair<TreeNode,Integer>>();
+        stack.push(new Pair<TreeNode,Integer>(root,START));
+        
+        while(!stack.isEmpty())
         {
-            if(node.left!=null)
-            {
-                TreeNode rightMostNode=node.left;
-                while(rightMostNode.right!=null)
-                {
-                    rightMostNode=rightMostNode.right;
-                }
-            rightMostNode.right= node.right;
-            node.right= node.left;
-            node.left=null;
-            }
-            node= node.right;
             
+            Pair<TreeNode,Integer> nodeData= stack.pop();
+            TreeNode currentNode= nodeData.getKey();
+            int recursionState=nodeData.getValue();
+            
+            
+             if (currentNode.left == null && currentNode.right == null) {
+                tailNode = currentNode;
+                continue;
             }
+            
+            if(recursionState==START)
+            {
+                
+                if(currentNode.left!=null)
+                {
+                    stack.push(new Pair<TreeNode,Integer>(currentNode,END));
+                    stack.push(new Pair<TreeNode,Integer>(currentNode.left,START));
+                    
+                }
+                
+                else if(currentNode.right!=null)
+                {
+                      stack.push(new Pair<TreeNode,Integer>(currentNode.right,START));
+                }
+                
+                
+                
+            }
+            
+            else
+            {
+                TreeNode rightNode = currentNode.right;
+                if(tailNode!=null)
+                {
+                    tailNode.right=currentNode.right;
+                    currentNode.right=currentNode.left;
+                    currentNode.left=null;
+                    rightNode=tailNode.right;
+                }
+                if(rightNode!=null)
+                         stack.push(new Pair<TreeNode, Integer>(rightNode, START));
+                
+                
+            }
+            
+            
+            
+            
+            
         }
         
+        
+        
     }
+}
