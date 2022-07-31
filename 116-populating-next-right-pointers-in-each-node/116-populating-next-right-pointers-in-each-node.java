4966f7a3d180c4a11e6625764d3d201bbd2e1a4b
Alfi Naaz <101350712+alfinaaz@users.noreply.github.com>
2022-07-31 16:54:55 +0530

    Time: 0 ms (100.00%), Space: 47.7 MB (38.04%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
@@ -24,30 +24,38 @@ class Node {
 class Solution {
     public Node connect(Node root) {
         
-        if(root==null)
+        if (root == null) {
             return root;
+        }
         
-        Queue<Node> q = new LinkedList<>();
-        q.add(root);
+        // Start with the root node. There are no next pointers
+        // that need to be set up on the first level
+        Node leftmost = root;
         
-        while(!q.isEmpty())
-        {
-            int size=q.size();
-            for(int i=0;i<size;i++)
-            {
-                Node node= q.poll();
+        // Once we reach the final level, we are done
+        while (leftmost.left != null) {
+            
+            // Iterate the "linked list" starting from the head
+            // node and using the next pointers, establish the 
+            // corresponding links for the next level
+            Node head = leftmost;
+            
+            while (head != null) {
                 
-                if(node.left!=null)
-                    q.add(node.left);
+                // CONNECTION 1
+                head.left.next = head.right;
                 
-                if(node.right!=null)
-                    q.add(node.right);
+                // CONNECTION 2
+                if (head.next != null) {
+                    head.right.next = head.next.left;
+                }
                 
-                if(i<size-1)
-                    node.next=q.peek();
-                
-                
+                // Progress along the list (nodes on the current level)
+                head = head.next;
             }
+            
+            // Move onto the next level
+            leftmost = leftmost.left;
         }
         
         return root;
