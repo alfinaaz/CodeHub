e4fc574e99d5d221f2705af56e6b7caa8e5efdc6
Alfi Naaz <101350712+alfinaaz@users.noreply.github.com>
2022-08-03 04:06:17 +0530

    Time: 2 ms (95.06%), Space: 41.9 MB (51.43%) - LeetHub

---
+++ 1628-design-an-expression-tree-with-evaluate-function/1628-design-an-expression-tree-with-evaluate-function.java
@@ -0,0 +1,112 @@
+/**
+ * This is the interface for the expression tree Node.
+ * You should not remove it, and you can define some classes to implement it.
+ */
+
+abstract class Node {
+    public abstract int evaluate();
+    // define your fields here
+};
+class TreeNode extends Node {
+    
+    String val;
+    Node left;
+    Node right;
+    int ans;
+    
+    public TreeNode(String val)
+    {
+        this.val=val;
+        left=null;
+        right=null;
+        
+        
+    }
+
+    // private String[] postfix;
+   // private static Set<String> op = Set.of("+", "-", "*", "/");
+
+  //  public TreeNode(String[] postfix) {
+    //    this.postfix = postfix;
+    //}
+
+    public int evaluate()
+    {
+        
+       return this.ans;
+        
+    }
+}
+
+
+
+/**
+ * This is the TreeBuilder class.
+ * You can treat it as the driver code that takes the postinfix input 
+ * and returns the expression tree represnting it as a Node.
+ */
+
+class TreeBuilder {
+    int idx=0;
+    Node buildTree(String[] postfix) {
+        String operators = "+-*/";
+        
+        Stack<TreeNode> st = new Stack<>();
+        while(idx<postfix.length)
+        {
+            if(operators.indexOf(postfix[idx])!=-1)
+            {
+                 TreeNode r=st.pop();
+                TreeNode l= st.pop();
+                TreeNode root= new TreeNode(postfix[idx]);
+                root.left=l;
+                root.right=r;
+                root.ans=calculate(l.ans,r.ans,postfix[idx]);
+                st.push(root);      
+            }
+            else
+            {
+                TreeNode nd= new TreeNode(postfix[idx]);
+                nd.ans=Integer.parseInt(postfix[idx]);
+                st.push(nd);
+            }
+            idx++;
+            
+        }
+        
+        return (Node)st.pop();
+        
+    }
+    public int calculate(int a,int b,String op)
+    {
+        
+   //     if(l.ans==Integer.MAX_VALUE)
+    //        l.ans=Integer.parseInt(l.val);
+        
+        
+    //    if(r.ans==Integer.MAX_VALUE)
+     //       r.ans=Integer.parseInt(r.val);
+        
+        
+         switch (op){
+            case "*":
+                return a*b;
+            case "/":
+                return a/b;
+            case "-":
+                return a-b;
+            default:
+                return a+b;
+                     
+    }
+       // return -1;
+};
+}
+
+
+/**
+ * Your TreeBuilder object will be instantiated and called as such:
+ * TreeBuilder obj = new TreeBuilder();
+ * Node expTree = obj.buildTree(postfix);
+ * int ans = expTree.evaluate();
+ */
