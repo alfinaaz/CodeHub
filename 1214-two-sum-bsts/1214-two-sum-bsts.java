1e0d342e93c64158edf68279e1b10a2d4071d9d4
Alfi Naaz <101350712+alfinaaz@users.noreply.github.com>
2022-08-06 03:54:21 +0530

    Time: 4 ms (81.20%), Space: 42.4 MB (97.52%) - LeetHub

--- 1214-two-sum-bsts/1214-two-sum-bsts.java
+++ 1214-two-sum-bsts/1214-two-sum-bsts.java
@@ -14,20 +14,35 @@
  * }
  */
 class Solution {
-    public boolean twoSumBSTs(TreeNode root1, TreeNode root2, int target) {
-         Set<Integer> s = inHashset(root1, target, new HashSet());
-        return inCheck(root2, s);
+     public boolean twoSumBSTs(TreeNode root1, TreeNode root2, int target) {
+    ArrayDeque<TreeNode> stack = new ArrayDeque();
+    Set<Integer> s = new HashSet();
+    // traverse the first tree 
+    // and store node complements (target - val) in hashset
+    while (!stack.isEmpty() || root1 != null) {
+      while (root1 != null) {
+        stack.push(root1);
+        root1 = root1.left;
+      }
+      root1 = stack.pop();
+      s.add(target - root1.val);
+      root1 = root1.right;
     }
-    public boolean inCheck(TreeNode r, Set<Integer> s) {
-    if (r == null) return false;
-    return inCheck(r.left, s) || s.contains(r.val) || inCheck(r.right, s);
-  }
-    public Set<Integer> inHashset(TreeNode r, int target, Set<Integer> s) {
-    if (r == null) return s;
-    inHashset(r.left, target, s);
-    s.add(target - r.val);
-    inHashset(r.right, target, s);
-    return s;
-  }
 
+    // traverse the second tree 
+    // and check if one of the values exists in hashset
+    while (!stack.isEmpty() || root2 != null) {
+      while (root2 != null) {
+        stack.push(root2);
+        root2 = root2.left;
+      }
+      root2 = stack.pop();
+      if (s.contains(root2.val)) {
+        return true;
+      }
+      root2 = root2.right;
+    }
+    
+    return false;
+  }
 }
