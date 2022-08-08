e49ee2ca77bbd2759468b0a33e646c9f8a4732ec
Alfi Naaz <101350712+alfinaaz@users.noreply.github.com>
2022-08-09 03:14:35 +0530

    Time: 7 ms (31.03%), Space: 44.3 MB (7.21%) - LeetHub

---
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,75 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ class Pair<T,V>{
+    
+    T first;
+    V second;
+    public Pair(T first, V second)
+    {
+        this.first=first;
+        this.second=second;
+    }
+}
+class Solution {
+   Map<Integer,List<Pair<Integer,Integer>>> map= new TreeMap<>();
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        
+        List<List<Integer>> ans= new ArrayList<>();
+        Queue<Pair<TreeNode,Pair<Integer,Integer>>> q= new LinkedList<>();
+        q.add(new Pair<TreeNode,Pair<Integer,Integer>>(root,new Pair<>(0,0)));
+        while(!q.isEmpty())
+        {
+            Pair<TreeNode,Pair<Integer,Integer>> p= q.remove();
+           
+                if(!map.containsKey(p.second.first))
+                {
+                    List<Pair<Integer,Integer>> arr= new ArrayList<>();
+                    arr.add(new Pair<Integer,Integer>(p.first.val,p.second.second));
+                    map.put(p.second.first,arr);
+                }
+                 else
+            {
+                List<Pair<Integer,Integer>> arr = map.get(p.second.first);
+                 arr.add(new Pair<Integer,Integer>(p.first.val,p.second.second));
+                 Collections.sort(arr, (p1, p2) ->p1.second==p2.second?p1.first-p2.first:p1.second-p2.second);
+                 map.put(p.second.first,arr);
+                
+            }
+            if(p.first.left!=null)
+            {
+                q.add(new Pair<TreeNode,Pair<Integer,Integer>>(p.first.left,new Pair<Integer,Integer>(p.second.first-1,p.second.second+1)));
+            }
+            if(p.first.right!=null)
+            {
+                 q.add(new Pair<TreeNode,Pair<Integer,Integer>>(p.first.right,new Pair<Integer,Integer>(p.second.first+1,p.second.second+1)));
+            }
+           
+        }
+        
+        for(Map.Entry<Integer,List<Pair<Integer,Integer>>> entry:map.entrySet())
+        {
+            List<Integer> list= new ArrayList<>();
+            for(int i=0;i<entry.getValue().size();i++)
+            {
+                list.add(entry.getValue().get(i).first);
+            }
+            ans.add(list);
+        }
+        return ans;
+        
+    }
+}
