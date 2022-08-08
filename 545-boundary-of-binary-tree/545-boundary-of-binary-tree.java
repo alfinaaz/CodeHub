c738803e83514a39f690deff844c08d6d5daec64
Alfi Naaz <101350712+alfinaaz@users.noreply.github.com>
2022-08-08 16:04:31 +0530

    Time: 1 ms (100.00%), Space: 42.3 MB (95.89%) - LeetHub

--- 545-boundary-of-binary-tree/545-boundary-of-binary-tree.java
+++ 545-boundary-of-binary-tree/545-boundary-of-binary-tree.java
@@ -13,44 +13,71 @@
  *     }
  * }
  */
-class Solution {
-    public List<Integer> boundaryOfBinaryTree(TreeNode root) {
-        List<Integer> ans=new ArrayList<>();
-        ans.add(root.val);
-        leftHelper(root.left,ans);
-        leaf(root.left,ans);
-        leaf(root.right,ans);
-        rightHelper(root.right,ans);
-        return ans;
-        
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+
+    public boolean isLeaf(TreeNode t) {
+        return t.left == null && t.right == null;
     }
-     public void leftHelper(TreeNode root,List<Integer> ans){
-        if(root==null) return;
-        if(root.left==null && root.right==null) return;
-        ans.add(root.val);
-        if(root.left!=null) leftHelper(root.left,ans);
-        else leftHelper(root.right,ans);
-    }
-    
-    public void leaf(TreeNode root,List<Integer> ans){
-        if(root==null) return;
-        if(root.left==null && root.right==null){
-            ans.add(root.val);
-            return;
+
+    public void addLeaves(List<Integer> res, TreeNode root) {
+        if (isLeaf(root)) {
+            res.add(root.val);
+        } else {
+            if (root.left != null) {
+                addLeaves(res, root.left);
+            }
+            if (root.right != null) {
+                addLeaves(res, root.right);
+            }
         }
-        leaf(root.left,ans);
-        leaf(root.right,ans);
     }
-    
-    
-    public void rightHelper(TreeNode root,List<Integer> ans){
-        if(root==null) return;
-        if(root.left==null && root.right==null) return;
-        if(root.right!=null) rightHelper(root.right,ans);
-        else rightHelper(root.left,ans);
-        ans.add(root.val);
+
+    public List<Integer> boundaryOfBinaryTree(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        if (!(root.left==null && root.right==null)) {
+            res.add(root.val);
+        }
+        TreeNode t = root.left;
+        while (t != null) {
+            if (!(t.left==null && t.right==null)) {
+                res.add(t.val);
+            }
+            if (t.left != null) {
+                t = t.left;
+            } else {
+                t = t.right;
+            }
+
+        }
+        addLeaves(res, root);
+        Stack<Integer> s = new Stack<>();
+        t = root.right;
+        while (t != null) {
+            if (!(t.left==null && t.right==null)) {
+                s.push(t.val);
+            }
+            if (t.right != null) {
+                t = t.right;
+            } else {
+                t = t.left;
+            }
+        }
+        while (!s.empty()) {
+            res.add(s.pop());
+        }
+        return res;
     }
-    
-    
-    
 }
