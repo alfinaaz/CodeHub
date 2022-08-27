554828f32e7d4919271dd34cfd10cc9a6bb929e9
Alfi Naaz <101350712+alfinaaz@users.noreply.github.com>
2022-08-28 01:08:45 +0530

    Time: 33 ms (81.82%), Space: 70 MB (67.80%) - LeetHub

--- 582-kill-process/582-kill-process.java
+++ 582-kill-process/582-kill-process.java
@@ -3,11 +3,11 @@ public class Solution {
     public List < Integer > killProcess(List < Integer > pid, List < Integer > ppid, int kill) {
         HashMap < Integer, List < Integer >> map = new HashMap < > ();
         for (int i = 0; i < ppid.size(); i++) {
-            if (ppid.get(i) > 0) {
+          //  if (ppid.get(i) > 0) {
                 List < Integer > l = map.getOrDefault(ppid.get(i), new ArrayList < Integer > ());
                 l.add(pid.get(i));
                 map.put(ppid.get(i), l);
-            }
+            //}
         }
         Queue < Integer > queue = new LinkedList < > ();
         List < Integer > l = new ArrayList < > ();
