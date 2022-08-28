e2dd32ab10f5b36bfdfb2b4fe07d9bc32a3bc393
Alfi Naaz <101350712+alfinaaz@users.noreply.github.com>
2022-08-28 16:58:03 +0530

    Time: 32 ms (83.55%), Space: 50.7 MB (93.28%) - LeetHub

--- 582-kill-process/582-kill-process.java
+++ 582-kill-process/582-kill-process.java
@@ -1,24 +1,32 @@
-public class Solution {
-
-    public List < Integer > killProcess(List < Integer > pid, List < Integer > ppid, int kill) {
-        HashMap < Integer, List < Integer >> map = new HashMap < > ();
-        for (int i = 0; i < ppid.size(); i++) {
-          //  if (ppid.get(i) > 0) {
-                List < Integer > l = map.getOrDefault(ppid.get(i), new ArrayList < Integer > ());
-                l.add(pid.get(i));
-                map.put(ppid.get(i), l);
-            //}
+class Solution {
+    List<Integer> list= new ArrayList<>();
+    public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {
+        HashMap<Integer,List<Integer>> map= new HashMap<>();
+      for(int i=0;i<ppid.size();i++)
+        {
+            if(ppid.get(i)==0)
+                continue;
+            
+           List <Integer> l = map.getOrDefault(ppid.get(i), new ArrayList <Integer> ());
+            l.add(pid.get(i));
+            map.put(ppid.get(i),l);
+            
         }
-        Queue < Integer > queue = new LinkedList < > ();
-        List < Integer > l = new ArrayList < > ();
-        queue.add(kill);
-        while (!queue.isEmpty()) {
-            int r = queue.remove();
-            l.add(r);
-            if (map.containsKey(r))
-                for (int id: map.get(r))
-                    queue.add(id);
+        list.add(kill);
+        return dfs(map,kill);
+        
+    }
+    public List<Integer> dfs(HashMap<Integer,List<Integer>> map,int kill)
+    {
+        if(map.containsKey(kill)){
+        for(int ele : map.get(kill))
+        {
+            list.add(ele);
+            dfs(map,ele);
         }
-        return l;
+        
+        }
+           
+           return list;
     }
 }
