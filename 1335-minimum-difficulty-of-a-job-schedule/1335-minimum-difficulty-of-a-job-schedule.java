529ec90af6a751908ab72b9ab40b9acfa6868e24
Alfi Naaz <101350712+alfinaaz@users.noreply.github.com>
2022-09-07 01:07:51 +0530

    Time: 48 ms (16.94%), Space: 42 MB (51.89%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.java
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.java
@@ -2,56 +2,55 @@ class Solution {
     public int minDifficulty(int[] jobDifficulty, int d) {
         
         int n=jobDifficulty.length;
-        
-        //Edge Case :
         if(n<d)
-        {
             return -1;
-        }
         
-        int[][] mem= new int[n][d+1];
+        int[][] mem= new int[n+1][d+1];
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<=d;j++)
+            for(int j=0;j<d+1;j++)
             {
                 mem[i][j]=-1;
             }
         }
         
         return minDiff(0,n,d,jobDifficulty,mem);
-        
-        
-        
+         
     }
-    
-    public int minDiff(int i,int n,int d,int[] jobDifficulty,int[][] mem)
+    public int minDiff(int i,int n,int d, int[] jobDifficulty,int[][] mem)
     {
         
-        
-        if (mem[i][d] != -1) 
+        if(mem[i][d]!=-1)
             return mem[i][d];
         
         if(d==1)
         {
-            int maxDiff=0;
+            int res=Integer.MIN_VALUE;
             for(int j=i;j<n;j++)
             {
-                maxDiff= Math.max(maxDiff,jobDifficulty[j]);
+                res= Math.max(res,jobDifficulty[j]);
             }
             
-            return maxDiff;
+            return res;
         }
-        int res=Integer.MAX_VALUE;
-        int dailyMaxJobDiff=0;
+        
+        int res= Integer.MAX_VALUE;
+        
+        
+        int maxDiff=0;
         for(int j=i;j<n-(d-1);j++)
         {
-            dailyMaxJobDiff=Math.max(dailyMaxJobDiff,jobDifficulty[j]);
-            res=Math.min(res,dailyMaxJobDiff+minDiff(j+1,n,d-1,jobDifficulty,mem));
+            maxDiff=Math.max(maxDiff,jobDifficulty[j]);
+            res=Math.min(res,maxDiff+minDiff(j+1,n,d-1,jobDifficulty,mem));
+            
+          
         }
-    
-         mem[i][d] = res;
-        return res;                 
+        mem[i][d]=res;
+        
+       
+        return res;
+        
+        
         
     }
-                         
 }
